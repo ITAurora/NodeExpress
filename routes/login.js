16eb4724a91b62e1dfe349c762c97ea0a5b9cf9d
ITAurora <691871826@qq.com>
2025-01-23 15:46:22 +0800

    修改

--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,13 @@ const express = require('express')
 
 //创建服务器
 const app = express.Router()
-const conn= require('../dataBase/db')
+const conn = require('../dataBase/db')
 var jwt = require("jsonwebtoken")
 
 app.post("/login", (req, res) => {
 	var userName = req.body.userName
 	var passWord = req.body.passWord
-	console.log(userName,passWord);
+	console.log(userName, passWord);
 	if (!userName || !passWord) {
 		res.send({
 			code: 0,
@@ -19,6 +19,8 @@ app.post("/login", (req, res) => {
 	}
 	const sqlStr = "select * from user WHERE userName=? AND passWord=?"
 	conn.query(sqlStr, [userName, passWord], (err, result) => {
+		console.log('result', result);
+
 		if (err) throw err
 		if (result.length > 0) {
 			// 生成token
@@ -28,26 +30,25 @@ app.post("/login", (req, res) => {
 					userName: result[0].userName,
 				},
 				"secret",
-				{ expiresIn: "1h" },
+				{ expiresIn: "1min" },
 			)
 			console.log(token)
 			res.send({ code: 1, msg: "登录成功", token: token })
- 
 			// 如果没有登录成功，则返回登录失败
 		} else {
-			// 判断token
-			if (req.headers.authorization == undefined || req.headers.authorization == null) {
-				if (req.headers.authorization) {
-					var token = req.headers.authorization.split(" ")[1] // 获取token
-				}
-				jwt.verify(token, "secret", (err, decode) => {
-					if (err) {
-						res.send({ code: 0, msg: "登录失败" })
-					}
-				})
-			}
+			res.send({ code: 0, msg: "账号或密码错误" })
 		}
 	})
 })
-
+app.post("/loginCheck", (req, res) => {
+	// 判断token
+	var token = req.body.token// 获取token
+	jwt.verify(token, "secret", (err, decode) => {
+		if (err) {
+			res.send({ code: 0, msg: "token验证失败" })
+		} else {
+			res.send({ code: 1, msg: "token验证成功" })
+		}
+	})
+})
 module.exports = app;
